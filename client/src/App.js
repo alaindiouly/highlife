8a42695d3c867eb35e45bb4eeff4d5d2481a190e
Alain Diouly-Osso <adiouly@yahoo.com>
2021-03-04 16:48:41 +0100

    basicAppJS

---
+++ client/src/App.js
@@ -0,0 +1,43 @@
+import './App.css';
+import './css-reset.css';
+import React, { useEffect , useState } from "react";
+import ReleaseList from "./containers/ReleaseList"
+import ApiService from './ApiService';
+
+
+function App() {
+  const [releases, setReleases] = useState([]);
+
+  useEffect(()=> {
+    ApiService.getSearch()
+    .then(res => {
+      const selectedReleases = res.results;
+      setReleases(selectedReleases)
+      return selectedReleases;
+    })
+
+    // REMOVE
+    .then(releases=>{
+      console.log(releases[0]);
+    })
+
+  }, []);
+
+  return (
+    <div className="App">
+      <h1>HEADERS</h1>
+      <h1>HERO</h1>
+      <h1>NAVBAR</h1>
+      <h1>AFROBEAT IS HERE!</h1>
+
+
+
+      <div className="list">
+        <ReleaseList id="list" releases={releases}/>
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
