fda27c6790117310fb1a9edea7da5b1668c3a009
Alain Diouly-Osso <adiouly@yahoo.com>
2021-03-04 21:58:08 +0100

    css size item cover

--- client/src/components/ReleaseItem.js
+++ client/src/components/ReleaseItem.js
@@ -4,26 +4,28 @@ function ReleaseItem ({release}) {
   //TO DO: link the release id to the artist(s)
 
   return (
-    <div className="wrapper">
-      <img src={release.cover_image} alt="" className="cover"/>
+    <div className="release">
+      <img src={release.cover_image} alt="" className="release__cover"/>
       <div className="title">
         {release.title}
       </div>
-      <div className="label">
+      {/* <div className="label">
         {release.label[0]}
-      </div>
+      </div> */}
       <div className="year">
         {release.year}
       </div>
-      <div className="country">
+      {/* <div className="country">
         {release.country}
-      </div>
-      <div className="have">
-        {release.community.have}
-      </div>
-      <div className="want">
-        {release.community.want}
-      </div>
+      </div> */}
+      <div className="release__community">
+        <div className="have">
+          Have:
+          {release.community.have+" "}
+          Want:
+          {release.community.want}
+        </div>
+       </div>
     </div>
   );
 }
